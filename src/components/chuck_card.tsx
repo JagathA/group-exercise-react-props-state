d00292d2470b8de2d006dc355f9c8c9cb5273fed
JagathA <jagath.ariy@gmail.com>
2023-02-02 16:12:09 +0000

    Requirement 1 done - Added greeting and Image

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,14 +1,20 @@
-// 💡 You can import assets like images directly and use them in your components:
-import ChuckImage from '../assets/chuck_norris.jpg';
+// 💡 You ccan import assets like images directly and use them in your components:
+import ChukImage from '../assets/chuck_norris.jpg';
 
-const ChuckCard = () =>
+interface chuckCardProps {
+	greeting: string;
+}
+
+
+const ChuckCard: React.FC<chuckCardProps> = ({ greeting }) => 
 	// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
 	// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
-	<>
-		<h2>{ }</h2>
 
-		<img src={'Chuck image here!'} alt='A handsome man' />
+	<>
+		<h2>{greeting }</h2>
+		<img src={ChukImage} alt='A handsome man' />
 	</>
+
 
 
 export default ChuckCard;
